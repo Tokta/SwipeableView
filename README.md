d9065ea5631a8da4022869a01666d355df0356d5
Alessio <alessio@lepaya.com>
2019-04-16 23:40:53 +0200

    Updated read me

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # SwipeableView
-SwipeView to recreate the UITableViewRowAction on every view
+
+SwipeView to recreate the UITableViewRowAction. You can customize every action with backgroundColor and UIImage easily. 
+
+![](SwipeDemo.gif)
+
+## Example
+
+Create SwipeableView:
+´´´
+let cell = SwipeableView(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: 50), backgroundColor: .white)
+
+´´´
+add an action:
+
+´´´
+cell.addAction(title: "Dog",
+image: UIImage(named: "dog") ?? UIImage(),
+backgroundcolor: UIColor.red) {
+self.showAlert("Dog \(num)")
+}
+
+´´´
+For futher instructions see the project demo.
+
+NOTE: SwipeView works on UITableViewCell as subview and also on UIScrollView. To use it on UITableView Header and Footer use it as subview of Header or Footer, do not pass it directly because it will be resized automatically and the swipe will broke. 
--- SwipeableView/ViewController.swift
+++ SwipeableView/ViewController.swift
@@ -25,8 +25,10 @@ class ViewController: UIViewController {
         var yPosition: CGFloat = 0
         for num in 1...20{
             
+            //Create a SwipeableView
             let cell = SwipeableView(frame: CGRect(x: 0, y: yPosition, width: scroll.frame.size.width, height: 50), backgroundColor: .white)
             
+            //Customize your SwipeableView
             let label = UILabel(frame: CGRect(x: 10, y: 0, width: scroll.frame.size.width - 20, height: 50))
             label.text = "- \(num)"
             label.textColor = .black
@@ -34,27 +36,50 @@ class ViewController: UIViewController {
             label.lineBreakMode = .byTruncatingTail
             cell.addSubview(label)
             
+            //add actions to the SwipeableView
             cell.addAction(title: "Dog",
                            image: UIImage(named: "dog") ?? UIImage(),
                            backgroundcolor: UIColor.red.withAlphaComponent(0.75)) {
-                            print("num \(num)")
+                            self.showAlert("Dog \(num)")
             }
             
             cell.addAction(title: "Fish",
                            image: UIImage(named: "fish") ?? UIImage(),
                            backgroundcolor: UIColor.blue.withAlphaComponent(0.75)) {
-                            print("num \(num)")
+                            self.showAlert("Fish \(num)")
             }
             
         
             cell.addAction(title: "Nothing",
                            image: nil,
                            backgroundcolor: UIColor.green.withAlphaComponent(0.75)) {
-                            print("num \(num)")
+                            self.showAlert("Nothing \(num)")
             }
             scroll.addSubview(cell)
             yPosition += cell.frame.size.height
+            
+            if num == 7 {
+                
+                //advanced customization of cell action
+                let action = cell.actions[0]
+                action.label.text = "Cat"
+                action.label.textColor = .red
+                action.backgroundColor = .yellow
+                
+                action.imageView?.backgroundColor = .white
+                
+            }
         }
         scroll.contentSize = CGSize(width: 10, height: yPosition)
+        
+        
+    }
+    
+    func showAlert(_ title: String){
+        
+        let alert = UIAlertController(title: title, message: "", preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
 }
